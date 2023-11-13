0075392cf2b4a7e19248037542d836fa9c68e530
VinhHuuTran <tranhuuvinh1109@gmail.com>
2023-11-13 13:22:45 +0700

    remove firebase-admin and call api to server main

--- src/app/controllers/FirebaseController.js
+++ src/app/controllers/FirebaseController.js
@@ -1,22 +1,27 @@
 const User = require("../models/User");
 const Data = require("../models/Data");
-const admin = require("firebase-admin");
-// const firebase = require('firebase');
-const serviceAccount = require("../../sevice.json");
-
-admin.initializeApp({
-  credential: admin.credential.cert(serviceAccount),
-  databaseURL:
-    "https://realtime-cnn-default-rtdb.asia-southeast1.firebasedatabase.app",
-});
-const database = admin.database();
+const axios = require("axios");
 
 class FirebaseController {
   // [POST] /realtime/update
   async update(req, res) {
     try {
       const { user_id, data } = req.body;
-      database.ref(`password/user_${user_id}`).update(data);
+      const updateObject = {};
+      data.forEach((element, index) => {
+        updateObject[index] = element;
+      });
+      axios
+        .post("https://server-deploy-xr7n.onrender.com/api/realtime/", {
+          user_id,
+          data: updateObject,
+        })
+        .then((response) => {
+          console.log("Response:", response.data);
+        })
+        .catch((error) => {
+          console.error("Error:", error.message);
+        });
 
       res.status(201).json({ message: "Data created successfully" });
     } catch (error) {
